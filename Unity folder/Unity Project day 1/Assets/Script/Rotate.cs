d238b7933c01198712b804f64bb67d87f1e2edde
Grajalesgirl875 <student@ent-v314-011.local>
2017-09-07 15:57:18 -0400

    Thursday 7

---
+++ Unity folder/Unity Project day 1/Assets/Script/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	//Public means rotation will show in Unity
+	//Vector3 stores X,Y,and Z information
+	public Vector3 myRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (myRotation * Time.deltaTime);
+	}
+}
