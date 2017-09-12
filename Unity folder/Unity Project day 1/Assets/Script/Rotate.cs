6afca83711939ce59a94d1428a1f48174e241b1a
Grajalesgirl875 <student@ent-v314-011.local>
2017-09-12 15:48:32 -0400

    Interaction

--- Unity folder/Unity Project day 1/Assets/Script/Rotate.cs
+++ Unity folder/Unity Project day 1/Assets/Script/Rotate.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour {
 
-	//Public means rotation will show in Unity
-	//Vector3 stores X,Y,and Z information
+	// if we say public before a variable type
+	// it will show up in the inspector
+
+	// Vector3 is a variable type that stores
+	// X, Y, and Z information
+
+	// in this case, we are naming the variable
+	// 'myRotation' so we know how to talk about it
 	public Vector3 myRotation;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (myRotation * Time.deltaTime);
--- Unity folder/Unity Project day 1/Assets/Script/Spawn.cs
+++ Unity folder/Unity Project day 1/Assets/Script/Spawn.cs
@@ -2,26 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn : MonoBehaviour {
+public class Spawner : MonoBehaviour {
 
+	// add a gameobject to the inspector
+	// that we can access in this script
 	public GameObject go;
 
+	// start spawning after... seconds
 	public float StartAfter = 1f;
 
+	// how often to spawn objects?
 	public float SpawnFrequency = .5f;
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("SpawnThing", StartAfter, SpawnFrequency);
+		// repeat a function
+		// 1: what you want to repeat (as a function)
+		// 2: after how many seconds do you want to start doing it?
+		// 3: repeat every X seconds
+		InvokeRepeating("SpawnThing", StartAfter, SpawnFrequency);
 	}
-	
+
 	// Update is called once per frame
 	void SpawnThing () {
 
-		transform.position = new Vector3 (Random.Range(-5,5), 3, Random.Range(-5,5));
+		// first, move the spawner
+		//		transform.position = new Vector3(Random.Range(-5f,5f), 3, Random.Range(-5f,5f));
 
-		Instantiate (go,transform.position, Quaternion.identity);
+		// to create an object, first use Instantiate function
+		// which wants 3 arguments:
+		// 1: the object you will create
+		// 2: WHERE to create that object
+		// 3: at what rotation
+		Instantiate (go, transform.position, Quaternion.identity);
 
-		Debug.Log (transform.position);
+		// print the location of the spawner object
+		Debug.Log(transform.position);
 	}
 }
