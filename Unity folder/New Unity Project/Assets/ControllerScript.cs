175305ff05dc463c7be7b5b38790d5ed038e9d0d
Grajalesgirl875 <student@ent-v314-011.local>
2017-11-14 15:50:35 -0500

    Weather1.Unity

---
+++ Unity folder/New Unity Project/Assets/ControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed; 
+	public float myvariable;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float MoveHorizontal = Input.GetAxis ("Horizontal");//listens to the key and moves it via the variable (Horizontal/Vertical (16/17)) 
+		float MoveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (MoveHorizontal, 0.0f, MoveVertical);
+
+		rb.AddForce(movement * speed); 
+			}
+}
